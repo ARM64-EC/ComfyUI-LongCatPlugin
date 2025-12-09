a75996c4e9b4fc72680c254cc5f5d3cdc550645a
ARM64_EC <tkl5503@psu.edu>
2025-12-09 01:56:55 -0500

    Fix: Remove obsolete nodes in __init__

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from .nodes import (
-    LongCatCheckpointLoader,
     TextEncodeLongCatImage,
     TextEncodeLongCatImageEdit,
     LongCatSizePicker,
@@ -8,7 +7,6 @@ from .nodes import (
 )
 
 NODE_CLASS_MAPPINGS = {
-    "LongCatCheckpointLoader": LongCatCheckpointLoader,
     "TextEncodeLongCatImage": TextEncodeLongCatImage,
     "TextEncodeLongCatImageEdit": TextEncodeLongCatImageEdit,
     "LongCatSizePicker": LongCatSizePicker,
@@ -17,7 +15,6 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LongCatCheckpointLoader": "LongCat Checkpoint Loader",
     "TextEncodeLongCatImage": "Text Encode LongCat Image",
     "TextEncodeLongCatImageEdit": "Text Encode LongCat Image Edit",
     "LongCatSizePicker": "LongCat Size Picker",
